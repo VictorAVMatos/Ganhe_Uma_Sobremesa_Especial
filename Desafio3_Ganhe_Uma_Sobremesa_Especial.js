7accda8144ce21478ff3e0852b854db13c46f849
Victor Alexandre Vieira de Matos <112444309+VictorAVMatos@users.noreply.github.com>
2023-05-11 19:50:25 -0300

    Add files via upload

---
+++ Desafio3_Ganhe_Uma_Sobremesa_Especial.js
@@ -0,0 +1,8 @@
+const valorPedido = parseInt(gets(90));
+
+if (valorPedido >= 50) {
+  print ('Parabens, você ganhou uma sobremesa gratis!')
+}
+  else {
+  print ('Que pena, você nao ganhou nenhum brinde especial.')
+}
